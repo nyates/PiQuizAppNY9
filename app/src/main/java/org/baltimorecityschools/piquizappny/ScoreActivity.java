88e860783ba0c3504d76c60aded4afc463b8d3e0
Yates <nyates@bcps.k12.md.us>
2024-09-13 14:20:48 -0400

    true/false buttons work with toasts, done button opens score activity

---
+++ app/src/main/java/org/baltimorecityschools/piquizappny/ScoreActivity.java
@@ -0,0 +1,32 @@
+package org.baltimorecityschools.piquizappny;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ScoreActivity extends AppCompatActivity {
+
+    TextView scoreTV;
+    int score;
+    Intent welcomeToSA;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+
+        scoreTV = findViewById(R.id.scoreTV);
+        score=0;
+        welcomeToSA = getIntent();
+        score = welcomeToSA.getIntExtra("score", 0);
+        scoreTV.setText("" + score);
+
+    }
+}
